205682b8ae11c15278ca32dbc181656b3d419610
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-04-16 23:08:50 +0200

    Build PromiseRace implementation

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 async function promiseAll(promises) {
   const values = [];
   for await (const p of promises) {
-      values.push(p)
+    values.push(p)
   }
 
   return values;
 }
 
-function promiseRace(promises) {
-  // Twój kod tu
+// I can't use await because await stops the js and waits for the value - but we want to run all promises and pick the first one ended.
+async function promiseRace(promises) {
+  return new Promise((res, rej) => {
+    for (const p of promises) {
+      typeof p === 'object' ? p.then(res, rej) : res(p);
+    }
+  });
 }
 
 // Kod testowy.
@@ -20,7 +25,7 @@ promiseAll([futureSuccess(1), futureSuccess(2), futureSuccess(3)]).then(result =
   console.log('To powinien być [1, 2, 3]:', result);
 });
 
-promiseAll([futureSuccess(1), futureSuccess(3), Promise.reject('X')])
+promiseAll([futureSuccess(1), Promise.reject('X'), futureSuccess(3)])
   .then(() => {
     console.log('WAT?! Nie powinno nas tu być..');
   })
@@ -31,33 +36,33 @@ promiseAll([futureSuccess(1), futureSuccess(3), Promise.reject('X')])
     console.log('To powinien być X:', error);
   });
 
-// promiseRace([1, 2, 3]).then(result => {
-//   console.log('This should be 1:', result);
-// });
+promiseRace([1, 2, 3]).then(result => {
+  console.log('This should be 1:', result);
+});
 
-// const now = performance.now();
-// promiseRace([delayedSuccess(1, 300), delayedSuccess(2, 200), delayedSuccess(3, 100)]).then(result => {
-//   const after = performance.now();
-//   const diff = after - now;
-//   if (diff < 100) {
-//     throw 'Za szybko!'
-//   }
-//   if (diff >= 200) {
-//     throw 'Za wolno!'
-//   }
-//   console.log('To powinno być 3:', result);
-// });
+const now = performance.now();
+promiseRace([delayedSuccess(1, 300), delayedSuccess(2, 200), delayedSuccess(3, 100)]).then(result => {
+  const after = performance.now();
+  const diff = after - now;
+  if (diff < 100) {
+    throw 'Za szybko!'
+  }
+  if (diff >= 200) {
+    throw 'Za wolno!'
+  }
+  console.log('To powinno być 3:', result);
+});
 
-// promiseRace([futureSuccess(1), Promise.reject('X'), futureSuccess(3)])
-//   .then(() => {
-//     console.log('WAT?! Nie powinno nas tu być..');
-//   })
-//   .catch(error => {
-//     if (error !== 'X') {
-//       console.log('Coś poszło nie tak..:', error);
-//     }
-//     console.log('To powinien być X:', error);
-//   });
+promiseRace([futureSuccess(1), Promise.reject('X'), futureSuccess(3)])
+  .then(() => {
+    console.log('WAT?! Nie powinno nas tu być..');
+  })
+  .catch(error => {
+    if (error !== 'X') {
+      console.log('Coś poszło nie tak..:', error);
+    }
+    console.log('To powinien być X:', error);
+  });
 
 function futureSuccess(val) {
   return new Promise((resolve) => {
